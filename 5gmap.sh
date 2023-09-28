f9abdddafee271208fa481e9c3172c554539d525
chiacchius <matteoch99@gmail.com>
2023-09-28 14:15:33 +0000

    try with zmq

--- 5gmap.sh
+++ 5gmap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "Starting UE..."
 rm -rf my_pipe
-pkill srsue
-pkill srsenb
+sudo pkill srsue
+sudo pkill srsenb
 sleep 1
 sudo python3 runner.py
--- 5gmap_sim.sh
+++ 5gmap_sim.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 export UE_VAR=ue
 if [ -n "$2" ] && [ -n "$3" ]; then
-    sudo -E .././build/srsue/src/srsue "$1" "$2" --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --gw.netns=ue1 > ./ue_logger 2>&1
+    sudo -E .././build/srsue/src/srsue "$1" "$2" --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --gw.netns=ue1 > ./tmp/ue_logger 2>&1
 else
-    sudo .././build/srsenb/src/srsenb --rf.device_name=zmq --rf.device_args="fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://localhost:2001,id=enb,base_srate=23.04e6" > ./enb_logger 2>&1 &
+    sudo .././build/srsenb/src/srsenb --rf.device_name=zmq --rf.device_args="fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://localhost:2001,id=enb,base_srate=23.04e6" > ./tmp/enb_logger 2>&1 &
     sleep 3
     sudo ip netns delete ue1
     sudo ip netns add ue1
-    sudo -E .././build/srsue/src/srsue "$1" --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --gw.netns=ue1 > ./ue_logger 2>&1
+    sudo -E .././build/srsue/src/srsue "$1" --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --gw.netns=ue1 > ./tmp/ue_logger 2>&1
 fi
 
 #sudo -E .././build/srsue/src/srsue "$1" --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --gw.netns=ue1
